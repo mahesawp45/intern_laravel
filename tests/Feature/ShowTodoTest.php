41b703f43df33c628b39c86db87940966d739002
mahesawp <kt.mahesa@gmail.com>
2022-10-17 13:59:36 +0800

    Commit ke coba branch 2

--- tests/Feature/ShowTodoTest.php
+++ tests/Feature/ShowTodoTest.php
@@ -20,39 +20,6 @@ class ShowTodoTest extends TestCase
 
     protected $todoRepository;
 
-
-    // public function __construct()
-    // {
-        //  $this->todoRepository = $this->mock(TodoRepository::class, function (MockInterface $mock) {
-        //     $mock->shouldReceive('getTodoById');
-        //  });
-        //  $this->todoRepository = new EloquentTodoRepository();
-    // }
-
-
-    // public function setRepo(TodoRepository $todoRepository) {
-    //     $this->todoRepository = $todoRepository;
-    // }
-
-    // public function test_show_or_get_todo_by_id()
-    // {
-
-    //     $this->withExceptionHandling();
-
-    //     // $todoModel = $this->createMock(Todo::class);
-    //     // $todoRepository = $this->createMock(TodoRepository::class);
-    //     // $eloquentTodo = $this->createMock(EloquentTodoRepository::class);
-
-    //     // $todoCont = new TodoController($todoRepository);
-    //     // $todoCont = new EloquentTodoRepository();
-
-    //     // $todo = $todoCont->getTodoById(1);
-    //     // $todoCont->show(1);
-
-    //     $response = $this->get(route('todo.show', ['todo' => 1]));
-    //     $response->assertStatus(200);
-
-    // }
     public function test_show_or_get_todo_by_id()
     {
 
@@ -65,9 +32,6 @@ class ShowTodoTest extends TestCase
                 ->shouldReceive('getTodoById')
                 ->with(Mockery::on(function ($argument) use ($todo) {
 
-                // var_dump($argument);
-                // exit();
-                // return $argument->is($todo->id);
                 return $argument;
             }));
         });
