eda65b9ae6a2f15cff9d99ec83bfc106fb09b2d6
mahesawp <kt.mahesa@gmail.com>
2022-10-17 14:58:34 +0800

    Conflict 2 dari main

--- tests/Feature/ShowTodoTest.php
+++ tests/Feature/ShowTodoTest.php
@@ -23,7 +23,7 @@ class ShowTodoTest extends TestCase
     public function test_show_or_get_todo_by_id()
     {
 
-        $this->withoutExceptionHandling();
+        $this->withCookie('name', 'Mahesa');
 
         $todo = Todo::factory()->create();
 
